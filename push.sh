ebac5b09cc5b902aedf3eebce31f91b4ba0ba126
Paulo Sousa <paulo.sousa@corp.globo.com>
2017-03-14 17:06:02 -0300

    only build for linux and GO_FOR_RELEASE // add goreleaser support

--- push.sh
+++ push.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+TRAVIS_GO_VERSION=$(echo $TRAVIS_GO_VERSION | sed -r 's/([0-9]+\.[0-9]+).*$/\1/')
+GO_FOR_RELEASE=$(echo $GO_FOR_RELEASE | sed -r 's/([0-9]+\.[0-9]+).*$/\1/')
+if ! [ "${TRAVIS_GO_VERSION}" = "${GO_FOR_RELEASE}" -a "${TRAVIS_OS_NAME}" = "linux" ]; then
+  echo "No package to build"
+  exit 0
+fi
+
 if [ ! -n "${TRAVIS_TAG}" -o ! -n "${PACKAGECLOUD_TOKEN}" -o ! -n "${PACKAGE_NAME}" ]; then
   echo "TRAVIS_TAG, PACKAGECLOUD_TOKEN and PACKAGE_NAME must be set"
   exit 1
 fi
+
+gem install fpm --no-ri --no-rdoc && curl -sL https://git.io/goreleaser | bash
 
 PACKAGE_CLOUD_REPO="tsuru/stable" 
 if [[ ${TRAVIS_TAG} =~ .+-rc ]]; then
