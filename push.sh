ae09991f3121b9f80505d17fde74f595ee09f102
Paulo Sousa <paulo.sousa@corp.globo.com>
2017-03-15 15:10:11 -0300

    add support for redhat, fedora, ubuntu and debian packages

--- push.sh
+++ push.sh
@@ -14,16 +14,42 @@ fi
 
 gem install fpm --no-ri --no-rdoc && curl -sL https://git.io/goreleaser | bash
 
-PACKAGE_CLOUD_REPO="tsuru/stable/ubuntu/trusty"
+ruby misc/fpm_recipe.rb
+
+PACKAGE_CLOUD_REPO="tsuru/stable"
 if [[ ${TRAVIS_TAG} =~ .+-rc ]]; then
-  PACKAGE_CLOUD_REPO="tsuru/rc/ubuntu/trusty"
+  PACKAGE_CLOUD_REPO="tsuru/rc"
 fi
+
+SUPPORTED_UBUNTU_VERSIONS="precise trusty xenial yakkety"
+SUPPORTED_DEBIAN_VERSIONS="jessie stretch buster"
+SUPPORTED_REDHAT_VERSIONS="6 7"
+SUPPORTED_FEDORA_VERSIONS=$(seq 14 25)
 
 gem install package_cloud --no-ri --no-rdoc
 if [ -f dist/${GORELEASE_PACKAGE_NAME}_${TRAVIS_TAG}_linux_amd64.deb ]; then
   mv dist/${GORELEASE_PACKAGE_NAME}_${TRAVIS_TAG}_linux_amd64.deb dist/${PACKAGE_NAME}_${TRAVIS_TAG}_amd64.deb
-  package_cloud push ${PACKAGE_CLOUD_REPO} dist/${PACKAGE_NAME}_${TRAVIS_TAG}_amd64.deb
 else
   echo "File dist/${PACKAGE_NAME}_${TRAVIS_TAG}_linux_amd64.deb not found."
   exit 1
 fi
+
+for ubuntu_version in ${SUPPORTED_UBUNTU_VERSIONS}
+do
+  package_cloud push ${PACKAGE_CLOUD_REPO}/ubuntu/${ubuntu_version} ${PACKAGE_NAME}_${TRAVIS_TAG}_amd64.deb
+done
+
+for debian_version in ${SUPPORTED_DEBIAN_VERSIONS}
+do
+  package_cloud push ${PACKAGE_CLOUD_REPO}/debian/${debian_version} ${PACKAGE_NAME}_${TRAVIS_TAG}_amd64.deb
+done
+
+for redhat_version in ${SUPPORTED_REDHAT_VERSIONS}
+do
+  package_cloud push ${PACKAGE_CLOUD_REPO}/el/${redhat_version} ${PACKAGE_NAME}-${TRAVIS_TAG/-/_}-1_.x86_64.rpm
+done
+
+for fedora_version in ${SUPPORTED_FEDORA_VERSIONS}
+do
+  package_cloud push ${PACKAGE_CLOUD_REPO}/fedora/${fedora_version} ${PACKAGE_NAME}-${TRAVIS_TAG/-/_}-1_.x86_64.rpm
+done
